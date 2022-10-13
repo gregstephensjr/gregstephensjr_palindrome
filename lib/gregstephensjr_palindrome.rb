a7c8a5d3e60a89d33f2796b164b85d1fe952e4ba
Greg Stephens Jr <gregstephensjr@users.noreply.github.com>
2022-10-13 16:44:20 -0600

    Finish working and refactored palindrome method

--- lib/gregstephensjr_palindrome.rb
+++ lib/gregstephensjr_palindrome.rb
@@ -2,7 +2,29 @@
 
 require_relative "gregstephensjr_palindrome/version"
 
+# module GregstephensjrPalindrome
+#   class Error < StandardError; end
+#   # Your code goes here...
+# end
+
 module GregstephensjrPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      self.to_s.scan(/[a-z\d]/i).join("").downcase
+    end
+end
+
+class String
+  include GregstephensjrPalindrome
+end
+
+class Integer 
+  include GregstephensjrPalindrome
 end
