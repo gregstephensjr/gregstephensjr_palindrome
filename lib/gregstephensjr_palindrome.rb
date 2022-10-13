395ed0658bc480330b6bd2bf6cc6d3357fd7c4ae
Greg Stephens Jr <gregstephensjr@users.noreply.github.com>
2022-10-13 10:54:17 -0600

    Initialize repository

---
+++ lib/gregstephensjr_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "gregstephensjr_palindrome/version"
+
+module GregstephensjrPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/gregstephensjr_palindrome/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module GregstephensjrPalindrome
+  VERSION = "0.1.0"
+end
